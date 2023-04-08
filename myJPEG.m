abcffcfb20821305b539dce24741cc39d5397981
Yoong Wei Jue <e0543600@u.nus.edu>
2023-04-09 00:57:32 +0800

    Add quantize functions to myJPEG

--- myJPEG.m
+++ myJPEG.m
@@ -76,6 +76,41 @@ classdef myJPEG
             imgBlocks = cellfun(@ifft2, fftImgBlocks, "UniformOutput", false);
         end
 
+        % Quantise values of each block and each channel
+        function quantized = quantize(fftImgBlocks, k)
+            q_table = [
+                16, 11, 10, 16, 24, 40, 51, 61;
+                12, 12, 14, 19, 26, 58, 60, 55;
+                14, 13, 16, 24, 40, 57, 69, 56;
+                14, 17, 22, 29, 51, 87, 80, 62;
+                18, 22, 37, 56, 68, 109, 103, 77;
+                24, 35, 55, 64, 81, 104, 113, 92;
+                49, 64, 78, 87, 103, 121, 120, 101;
+                72, 92, 95, 98, 112, 100, 103, 99
+            ];
+            q_table = kron(q_table, ones(k / 8));
+            quantized = cellfun(@(c) c ./ q_table, fftImgBlocks, "UniformOutput", false);
+            quantized = cellfun(@round, quantized, "UniformOutput", false);
+        end
+
+        % Get the unquantised values
+        function fftImgBlocks = unquantize(quantized, k)
+            q_table = [
+                16, 11, 10, 16, 24, 40, 51, 61;
+                12, 12, 14, 19, 26, 58, 60, 55;
+                14, 13, 16, 24, 40, 57, 69, 56;
+                14, 17, 22, 29, 51, 87, 80, 62;
+                18, 22, 37, 56, 68, 109, 103, 77;
+                24, 35, 55, 64, 81, 104, 113, 92;
+                49, 64, 78, 87, 103, 121, 120, 101;
+                72, 92, 95, 98, 112, 100, 103, 99
+            ];
+            q_table = kron(q_table, ones(k / 8));
+            fftImgBlocks = cellfun(@(c) c .* q_table, quantized, "UniformOutput", false);
+        end
+
+
+
         function compressedData = compress(rgbImg, quality)
             if strcmp(quality, "low")
                 sampleFactor = 0.25;
